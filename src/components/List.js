d39344348491a2d92a5d4d237dbfeebb141851d9
Pranay Agarwal <pranay12@connect.hku.hk>
2020-06-02 18:48:42 +0530

    todo block

--- src/components/List.js
+++ src/components/List.js
@@ -3,20 +3,26 @@ import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 
 const List = ({todos, toggleTodo, removeTodo})=>(
     <View style={styles.container}>
-        <Text style={{textAlign: "center", fontWeight: "bold", fontSize: 25}}>
+        <Text style={{textAlign: "center", fontWeight: "bold", fontSize: 25, backgroundColor: '#0f9'
+            }}>
             ToDo:
         </Text>
         {todos.map(todo =>{
-            return (<TouchableOpacity key={todo.id} onPress={()=>toggleTodo(todo.id)} 
-                onLongPress={()=>removeTodo(todo.id)}>
-                <Text style={
-                    {fontSize:20, 
-                    textDecorationLine: todo.completed===true?'line-through':'none',
-                    color: todo.completed===false?'black':'white'
-                }}>
-                {todo.text}
-                </Text>
-            </TouchableOpacity>);
+            return (
+                <TouchableOpacity key={todo.id} onPress={()=>toggleTodo(todo.id)} 
+                    onLongPress={()=>removeTodo(todo.id)}>
+                        <View style={{backgroundColor: !todo.completed?'rgba(0, 256, 160, 1)':'rgba(150, 150, 150, 0.9)', 
+                            height: 45, paddingHorizontal: 20, 
+                            paddingVertical: 10, borderWidth: 1, borderColor: '#000', borderBottomWidth: 0.5}}>
+                        <Text style={
+                            {fontSize:20, 
+                            textDecorationLine: todo.completed===true?'line-through':'none',
+                            color: todo.completed===false?'black':'white'}}>
+                            {todo.text}
+                        </Text>
+                        </View>
+                </TouchableOpacity>
+            );
         })}
     </View>
 )
@@ -25,12 +31,11 @@ export default List;
 const styles = StyleSheet.create({
     container: {
       backgroundColor: '#fff',
-      paddingHorizontal: 20,
-      paddingVertical: 10,
+    //   paddingHorizontal: 20,
+    //   paddingVertical: 10,
       marginHorizontal:15,
       borderWidth:2,
       marginTop:25,
       borderColor: '#0c6',
-      backgroundColor: '#0f9'
     },
   });
